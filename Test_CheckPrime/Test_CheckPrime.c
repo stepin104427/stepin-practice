4fc8402300a3e2b1cd45fd78eaf8e3b28f051b74
Milind Mohapatra <mohapatra.milind@gmail.com>
2020-08-11 03:49:41 +0530

    Add main function

--- Test_CheckPrime/Test_CheckPrime.c
+++ Test_CheckPrime/Test_CheckPrime.c
@@ -1,2 +1,33 @@
 #include "unity.h"
 #include "CheckPrime.h"
+
+void setUp(){}
+
+void tearDown)(){}
+
+void testPrime(void)
+{
+  TEST_ASSERT_EQUAL(1, checkPrime(37));
+  TEST_ASSERT_EQUAL(1, checkPrime(97));
+  TEST_ASSERT_EQUAL(0, checkPrime(10));
+}
+void testOneZero(void)
+{
+  TEST_ASSERT_EQUAL(0, checkPrime(1));
+  TEST_ASSERT_EQUAL(0, checkPrime(0));
+}
+void testNegative(void)
+{
+  TEST_ASSERT_EQUAL(0, checkPrime(-2));
+}
+
+int test_main(void)
+{
+  UNITY_BEGIN();
+
+  RUN_TEST(testPrime);
+  RUN_TEST(testOneZero);
+  RUN_TEST(testNegative);
+
+  return UNITY_END();
+}
