f5a2b10293ae7d1cfc3f9b57f1be3a9cf650ddca
Milind Mohapatra <mohapatra.milind@gmail.com>
2020-08-12 04:05:58 +0530

    Add documentation

--- test/Test_CheckPrime.h
+++ test/Test_CheckPrime.h
@@ -1,6 +1,16 @@
+/**
+* @file Test_CheckPrime.h
+*
+*/
 #ifndef __TEST_CHECKPRIME_H__
 #define __TEST_CHECKPRIME_H__
 
+/**
+* Unit testing for checkPrime().
+* Test Case 1: Standard numbers\n
+* Test Case 2: Zero and One\n
+* Test Case 3: Negative numbers
+*/
 int testMain(void);
 
 #endif
