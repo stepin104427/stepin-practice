9c35a215bc88aa568650144d98fe5242d09cefa4
Milind Mohapatra <mohapatra.milind@gmail.com>
2020-08-11 21:11:04 +0530

    Initial commit

---
+++ inc/CheckPrime.h
@@ -0,0 +1,16 @@
+/**
+* @file CheckPrime.h
+*
+*/
+#ifndef __CHECKPRIME_H__
+#define __CHECKPRIME_H__
+
+/**
+* Check if a number is prime or note
+* @param[in] Number to be checked
+* @return If number is prime or note
+* @note Returns 1 if prime and 0 if not prime
+*/
+int CheckPrime(int n);
+
+#endif
