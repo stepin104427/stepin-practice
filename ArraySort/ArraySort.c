8438fdd48b73a8a90788a79686d4714895dca336
Milind Mohapatra <mohapatra.milind@gmail.com>
2020-08-13 02:29:33 +0530

    remove main function

--- ArraySort/ArraySort.c
+++ ArraySort/ArraySort.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 void swap(int *num1, int *num2)
 {
@@ -22,33 +21,4 @@ void sort(int *array)
       }
     }
   }
-}
-
-int main()
-{
-  int length = 0;
-  printf("Enter number of elements: ");
-  scanf("%d", &length);
-  int *array = malloc(sizeof(int) * (length + 1));
-  array[0] = length;
-
-  printf("Enter %d integers:\n", length);
-  for(int i = 1; i <= length; i++)
-  {
-    scanf("%d", (array + i));
-  }
-
-  printf("Original array[length = %d]: ", *array);
-  for(int i = 1; i <= length; i++)
-  {
-    printf("%d ", *(array + i));
-  }
-  sort(array);
-  printf("\nSorted array: ");
-  for(int i = 1; i <= length; i++)
-  {
-    printf("%d ", *(array + i));
-  }
-  printf("\n");
-  return 0;
 }
